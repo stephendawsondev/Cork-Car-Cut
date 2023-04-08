a62af8cb174ab85e4dd72e28f5a871b21a71e0ec
Stephen Dawson <stephendawson@protonmail.com>
2023-04-08 12:47:55 +0000

    format main.js, add event to close menu when escape is pressed, edit dropdown style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,30 @@
 // select menu dropdown item
 const dropdownParent = document.querySelector('.nav__item.dropdown > a');
-const dropdownList = document.querySelector('.nav__item.dropdown > .dropdown-list');
-
 
 // add the click event listener to toggle the menu item
 dropdownParent.addEventListener('click', event => {
     // prevent clicking the parent from navigating to a new page
     event.preventDefault();
 
-    let dropdownActive = dropdownParent.classList.contains('dropdown--active')
+    let dropdownActive = dropdownParent.classList.contains('dropdown--active');
 
     // if the dropdown is active then set aria-expanded to false
     // otherwise set it to true
-    if(dropdownActive){
-        dropdownParent.setAttribute('aria-expanded', 'false')
+    if (dropdownActive) {
+        dropdownParent.setAttribute('aria-expanded', 'false');
     } else {
-        dropdownParent.setAttribute('aria-expanded', 'true')
+        dropdownParent.setAttribute('aria-expanded', 'true');
     }
-    
+
     // this adds the class if not present then removes it if it is present
-    dropdownParent.classList.toggle('dropdown--active')
-})
+    dropdownParent.classList.toggle('dropdown--active');
+});
+
+// add an event listener to the document so that the escape key closes the dropdown
+document.addEventListener('keydown', event => {
+    // check if the pressed key is the escape key
+    if (event.key === "Escape") {
+        dropdownParent.classList.remove('dropdown--active');
+        dropdownParent.setAttribute('aria-expanded', 'false');
+    }
+});
